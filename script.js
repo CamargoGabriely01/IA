4810d6900d721bd09347868ce98a1693d23a8e1f
CamargoGabriely01 <168448630+CamargoGabriely01@users.noreply.github.com>
2025-05-30 10:51:02 -0300

    ok

--- script.js
+++ script.js
@@ -4,47 +4,60 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
+const perguntas[
     {
         enunciado: "Pergunta1",
         alternativas: [
             {
-                texto: "alternativa1",
-                afirmacao: "afirmacao1"
+                texto: "alternativa1", 
+                afirmacao: "afirmacao1",
             },
             {
-                texto: "alternativa2",
-                afirmacao: "afirmacao2"
-            }
-        ],
-
+                texto: "alternativa2", 
+                afirmacao: "afirmacao2",
+            },
+        ]
     },
+
     {
+         {
         enunciado: "Pergunta2",
         alternativas: [
             {
-                texto: "alternativa1",
-                afirmacao: "afirmacao1"
+                texto: "alternativa1", 
+                afirmacao: "afirmacao1",
             },
             {
-                texto: "alternativa2",
-                afirmacao: "afirmacao2"
-            }
-        ],
-
+                texto: "alternativa2", 
+                afirmacao: "afirmacao2",
+            },
+        ]
     },
-    {
+    },
+     {
         enunciado: "Pergunta3",
         alternativas: [
             {
-                texto: "alternativa1",
-                afirmacao: "afirmacao1"
+                texto: "alternativa1", 
+                afirmacao: "afirmacao1",
             },
             {
-                texto: "alternativa2",
-                afirmacao: "afirmacao2"
-            }
-        ],
-
+                texto: "alternativa2", 
+                afirmacao: "afirmacao2",
+            },
+        ]
     },
 ]
+
+let atual = 0;
+let perguntaAtual;
+
+function motraPrgunta (){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+
+}
