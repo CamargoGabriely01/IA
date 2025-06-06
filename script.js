d7949cb15afd76b54e7fb006ca299f620ac1c7ed
CamargoGabriely01 <168448630+CamargoGabriely01@users.noreply.github.com>
2025-06-06 11:15:27 -0300

    ok

--- script.js
+++ script.js
@@ -6,86 +6,77 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Como a conscientização sobre a homofobia pode ser integrada às celebrações da Semana Farroupilha, promovendo a diversidade cultural do Rio Grande do Sul?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: ["No início ficou com medo do que essa tecnologia pode fazer. ",
-                    "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+                texto: "Podemos promover palestras e debates sobre inclusão e respeito Noà diversidade sexual em piquetes e eventos tradicionalistas.",
+                afirmacao: [" Essas ações ajudariam a desmistificar preconceitos e a fortalecer a ideia de que a tradição gaúcha é acolhedora para todos.",
+                    
                 ]
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: ["Quis saber como usar IA no seu dia a dia.",
-                    "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia."
+                texto: "É possível desenvolver materiais educativos e campanhas informativas que utilizem elementos da cultura gaúcha.",
+                afirmacao: ["Isso permitiria transmitir mensagens de combate à homofobia, alcançando um público amplo e consolidando valores de acolhimento.",
+                   
                 ]
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Qual a importância de abordar a questão da homofobia ao discutir a preservação da fauna e flora nativas do Rio Grande do Sul, usando a Semana Farroupilha como pano de fundo?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: ["Conseguiu utilizar a IA para buscar informações úteis.",
-                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
-                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                texto: "Ao defender a diversidade biológica, reforçamos o valor da diversidade em todas as suas formas.",
+                afirmacao: ["Combater a homofobia alinha-se com a premissa de um ecossistema social inclusivo, onde todas as espécies (pessoas) são importantes para o equilíbrio.",
                 ]
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: ["Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
-                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
-                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                texto: "Celebrar a Semana Farroupilha é valorizar a identidade gaúcha, que inclui um profundo respeito pela terra.",
+                afirmacao: ["Essa valorização deve se estender a todas as formas de vida e a todos os indivíduos, independentemente de sua orientação sexual, para uma sociedade verdadeiramente harmoniosa e preservacionista."
                 ]
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Durante a Semana Farroupilha, de que forma a valorização das tradições pode ser utilizada para promover a empatia e combater a homofobia, mantendo a essência cultural gaúcha?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: ["Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
-                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                texto: "Podemos destacar os valores de camaradagem, hospitalidade e união que são inerentes à cultura gaúcha.",
+                afirmacao: ["Esses valores podem ser expandidos para incluir o respeito a todas as pessoas, independentemente de sua orientação sexual, mostrando que a tradição é inclusiva."
                 ]
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: ["Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
-                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                texto: "É possível incorporar a temática da diversidade através de manifestações artísticas e culturais nos eventos farroupilhas, como peças teatrais ou apresentações musicais.",
+                afirmacao: ["Essa abordagem sensibiliza o público de forma orgânica e positiva, promovendo a aceitação sem perder a autenticidade cultural."
                 ]
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Considerando a urgência da extinção de animais, como a celebração da Semana Farroupilha pode inspirar ações de conservação que também ressoem com a luta contra a homofobia?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: ["Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes.",
-                    "Ainda acha que os meios de desenho tradicionais são mais eficazes para a criatividade, por isso vem estimulando pessoas em suas redes sociais a fazer pintura em aquarela."
+                texto: "Ao celebrar a riqueza da cultura gaúcha, podemos estender essa valorização à riqueza da biodiversidade local.",
+                afirmacao: ["Promover a conservação de espécies ameaçadas e, ao mesmo tempo, lutar contra a homofobia são atos de respeito à vida em suas diversas manifestações."
                 ]
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: ["Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!",
-                    "Compartilhou artes em redes sociais como forma de ensinar como se comunicar através da arte.",
-                    "Percebeu que muitas pessoas têm dificuldade em expressar suas ideias desenhando e acha que a IA é capaz de empoderar essas pessoas a tirarem ideias do papel."
+                texto: "A Semana Farroupilha representa um momento de união e mobilização da comunidade.",
+                afirmacao: ["Essa energia pode ser canalizada para projetos de conservação ambiental que também abordem a importância da diversidade social, conectando a preservação do meio ambiente com a construção de uma sociedade mais justa e inclusiva."
                 ]
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "De que maneira a luta contra a extinção de espécies e o combate à homofobia compartilham princípios éticos que podem ser enfatizados durante a Semana Farroupilha? ",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Ambos os movimentos são fundamentalmente baseados no respeito à vida e na valorização da diversidade.",
+                afirmacao: "Enfatizar a importância de proteger todas as formas de vida (sejam espécies animais ou indivíduos LGBTQIA+) é um princípio ético universal que se alinha com os valores de humanidade e justiça."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "A ideia de preservar o que é nosso na Semana Farroupilha pode ser expandida para preservar o que é essencial para todos.",
+                afirmacao: "Isso inclui tanto a biodiversidade do planeta quanto o direito de todas as pessoas existirem e serem respeitadas, fortalecendo a coesão social e ambiental."
             }
         ]
     },
